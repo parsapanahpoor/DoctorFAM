cd8309994e05d6c7dbab99f2fffb54665d9e7c5b
parsa panahpoor <parsapanahpoor77@yahoo.com>
2022-10-31 16:13:26 +0330

    Compelete PWA task

---
+++ DoctorFAM.Presentation/wwwroot/Site/ServiceWorker/Index.js
@@ -0,0 +1,14 @@
+﻿
+window.addEventListener('load', () => {
+    registerSW();
+});
+
+async function registerSW() {
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator.serviceWorker.register('/sw.js');
+        } catch (e) {
+            console.log(`SW registration failed`);
+        }
+    }
+}
---
+++ DoctorFAM.Presentation/wwwroot/Site/js/PWADownloader.js
@@ -0,0 +1,31 @@
+﻿
+let deferredPrompt;
+var div = document.querySelector('.add-to');
+var button = document.querySelector('.add-to-btn');
+div.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    div.style.display = 'block';
+
+    button.addEventListener('click', (e) => {
+        // hide our user interface that shows our A2HS button
+        div.style.display = 'none';
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice
+            .then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the A2HS prompt');
+                } else {
+                    console.log('User dismissed the A2HS prompt');
+                }
+                deferredPrompt = null;
+            });
+    });
+});
+
