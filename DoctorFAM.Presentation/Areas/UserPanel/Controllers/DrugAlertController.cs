05ceb123f737cbc5d4c0442d01687c4fb5371e49
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-01-22 12:29:48 +0330

    Add 3 Part Of Health Care To The User Panel

---
+++ DoctorFAM.Presentation/Areas/UserPanel/Controllers/DrugAlertController.cs
@@ -0,0 +1,47 @@
+﻿using DoctorFAM.Application.Extensions;
+using DoctorFAM.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorFAM.Web.Areas.UserPanel.Controllers
+{
+    public class DrugAlertController : UserBaseController
+    {
+        #region Ctor
+
+        private readonly IDrugAlertService _drugAlertService;
+
+        public DrugAlertController(IDrugAlertService drugAlertService)
+        {
+            _drugAlertService = drugAlertService;
+        }
+
+        #endregion
+
+        #region List Of Current User Drug Alerts
+
+        [HttpGet]
+        public async Task<IActionResult> ListOfCurrentUserDrugAlerts()
+        {
+            return View(await _drugAlertService.FillListOfUserDrugAlertsSiteSideViewModel(User.GetUserId()));
+        }
+
+        #endregion
+
+        #region Delete Drug Alert 
+
+        public async Task<IActionResult> DeleteDrugAlert(ulong drugAlertId)
+        {
+            var res = await _drugAlertService.DeleteDrugAlert(drugAlertId, User.GetUserId());
+            if (res)
+            {
+                TempData[SuccessMessage] = "عملیات باموفقیت انجام شده است.";
+                return RedirectToAction(nameof(ListOfCurrentUserDrugAlerts));
+            }
+
+            TempData[ErrorMessage] = "عملیات باشکست مواجه شده است.";
+            return RedirectToAction(nameof(ListOfCurrentUserDrugAlerts));
+        }
+
+        #endregion
+    }
+}
---
+++ DoctorFAM.Presentation/Areas/UserPanel/Controllers/PeriodicTestController.cs
@@ -0,0 +1,56 @@
+﻿using DoctorFAM.Application.Extensions;
+using DoctorFAM.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorFAM.Web.Areas.UserPanel.Controllers
+{
+    public class PeriodicTestController : UserBaseController
+    {
+        #region Ctor
+
+        private readonly IPeriodicTestService _periodicTestService;
+
+        public PeriodicTestController(IPeriodicTestService periodicTestService)
+        {
+            _periodicTestService = periodicTestService;
+        }
+
+        #endregion
+
+        #region List Of Periodic Tests 
+
+        [HttpGet]
+        public async Task<IActionResult> ListOfUserPeriodicTest()
+        {
+            #region Fill Model 
+
+            var model = await _periodicTestService.GetListOFUserPeriodicTestByUserId(User.GetUserId());
+            if (model == null) return NotFound();
+
+            #endregion
+
+            return View(model);
+        }
+
+        #endregion
+
+        #region Delete User Periodic Test
+
+        public async Task<IActionResult> DeleteUserPeriodicTest(ulong id)
+        {
+            var res = await _periodicTestService.DeleteUserPeriodicSelectedTest(id, User.GetUserId());
+
+            if (res)
+            {
+                TempData[SuccessMessage] = "عملیات باموفقیت انجام شده است.";
+                return RedirectToAction(nameof(ListOfUserPeriodicTest));
+            }
+
+            TempData[ErrorMessage] = "اطلاعات وارد شده صحیح نمی باشد.";
+            return RedirectToAction(nameof(ListOfUserPeriodicTest));
+        }
+
+        #endregion
+    }
+}
