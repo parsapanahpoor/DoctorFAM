37c9b68633b15514927729ae5ced43c3bc04c2a0
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-05-11 11:52:16 +0330

    add field

--- DoctorFAM.Domain/ViewModels/Site/OnlineVisit/ListOfShiftSiteSideViewModel.cs
+++ DoctorFAM.Domain/ViewModels/Site/OnlineVisit/ListOfShiftSiteSideViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoctorFAM.Domain.Entities.Books;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,8 @@ namespace DoctorFAM.Domain.ViewModels.Site.OnlineVisit
         public ulong WorkShiftDateTimeId { get; set; }
 
         public ulong WorkShiftDateId { get; set; }
+
+        public bool IsFinaly { get; set; }
 
         #endregion
     }
