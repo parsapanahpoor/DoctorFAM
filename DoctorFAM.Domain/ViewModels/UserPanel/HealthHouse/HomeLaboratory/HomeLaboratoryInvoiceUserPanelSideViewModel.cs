e81bfe293d12e739c3de8799313d5427de14f401
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-05-29 20:16:28 +0330

    before HomeLaboratoryRequestPrice table

--- DoctorFAM.Domain/ViewModels/UserPanel/HealthHouse/HomeLaboratory/HomeLaboratoryInvoiceUserPanelSideViewModel.cs
+++ DoctorFAM.Domain/ViewModels/UserPanel/HealthHouse/HomeLaboratory/HomeLaboratoryInvoiceUserPanelSideViewModel.cs
@@ -1,24 +1,27 @@
-﻿using System;
+﻿#region Usings
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DoctorFAM.Domain.ViewModels.UserPanel.HealthHouse.HomeLaboratory
+#endregion
+
+namespace DoctorFAM.Domain.ViewModels.UserPanel.HealthHouse.HomeLaboratory;
+
+public class HomeLaboratoryInvoiceUserPanelSideViewModel
 {
-    public class HomeLaboratoryInvoiceUserPanelSideViewModel
-    {
-        #region properties
+    #region properties
 
-        public ulong RequestId { get; set; }
+    public ulong RequestId { get; set; }
 
-        public int Price { get; set; }
+    public int Price { get; set; }
 
-        public string InvoicePicFileName { get; set; }
+    public string InvoicePicFileName { get; set; }
 
-        public bool IsFinalize { get; set; }
+    public bool IsFinalize { get; set; }
 
-        #endregion
-    }
+    #endregion
 }
