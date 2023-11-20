5cea62f898e417074be185a2a8ee03a77ba89c37
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-11-20 23:18:56 +0330

    After Add ContractWithFamilyDoctors

--- DoctorFAM.Domain/Entities/Doctors/DoctorsInfo.cs
+++ DoctorFAM.Domain/Entities/Doctors/DoctorsInfo.cs
@@ -1,61 +1,56 @@
-﻿using DoctorFAM.Domain.Entities.Account;
-using DoctorFAM.Domain.Entities.Common;
+﻿using DoctorFAM.Domain.Entities.Common;
 using DoctorFAM.Domain.Enums.DoctorTitleName;
 using DoctorFAM.Domain.Enums.Gender;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DoctorFAM.Domain.Entities.Doctors
+namespace DoctorFAM.Domain.Entities.Doctors;
+
+public sealed class DoctorsInfo : BaseEntity
 {
-    public class DoctorsInfo : BaseEntity
-    {
-        #region properties
+    #region properties
 
-        public ulong DoctorId { get; set; }
+    public ulong DoctorId { get; set; }
 
-        public string NationalCode { get; set; }
+    public string NationalCode { get; set; }
 
-        public string MedicalSystemCode { get; set; }
+    public string MedicalSystemCode { get; set; }
 
-        public string Education { get; set; }
+    public string Education { get; set; }
 
-        public string? Specialty { get; set; }
+    public string? Specialty { get; set; }
 
-        public string MediacalFile { get; set; }
+    public string MediacalFile { get; set; }
 
-        public Gender Gender { get; set; }
+    public Gender Gender { get; set; }
 
-        public string? GeneralPhone  { get; set; }
+    public string? GeneralPhone  { get; set; }
 
-        public string? ClinicPhone { get; set; }
+    public string? ClinicPhone { get; set; }
 
-        [Display(Name = "تعداد پیامک رایگان برای ارسال از پزشک به بیماران")]
-        public int CountOFFreeSMSForDoctors { get; set; }
+    [Display(Name = "تعداد پیامک رایگان برای ارسال از پزشک به بیماران")]
+    public int CountOFFreeSMSForDoctors { get; set; }
 
-        public DoctorTilteName DoctorTilteName { get; set; } = DoctorTilteName.Anonymous;
+    public DoctorTilteName DoctorTilteName { get; set; } = DoctorTilteName.Anonymous;
 
-        #endregion
+    public bool ContractWithFamilyDoctors { get; set; }
 
-        #region Relations
+    #endregion
 
-        public Doctor Doctor { get; set; }
+    #region Relations
 
-        #endregion
-    }
+    public Doctor Doctor { get; set; }
 
-    public enum OrganizationInfoState
-    {
-        [Display(Name = "Accepted")]
-        Accepted,
-        [Display(Name = "WaitingForConfirm")]
-        WatingForConfirm,
-        [Display(Name = "Rejected")]
-        Rejected,
-        [Display(Name = "Register Now")]
-        JustRegister
-    }
+    #endregion
+}
+
+public enum OrganizationInfoState
+{
+    [Display(Name = "Accepted")]
+    Accepted,
+    [Display(Name = "WaitingForConfirm")]
+    WatingForConfirm,
+    [Display(Name = "Rejected")]
+    Rejected,
+    [Display(Name = "Register Now")]
+    JustRegister
 }
