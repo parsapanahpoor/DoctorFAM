f779a846ceecd0e9b10ab5e5503820fcc26e9ddf
parsapanahpoor <parsapanahpoor77@gmail.com>
2024-06-02 00:23:01 +0330

    Add AddBloodPressurePopulationCommandHandler

---
+++ DoctorFAM.Application/CQRS/SiteSide/BloodPressure/Command/AddBloodPressurePopulation/AddDiabetPopulationCommand.cs
@@ -0,0 +1,9 @@
+﻿using DoctorFAM.Domain.ViewModels.Site.BloodPressure;
+using System.Windows.Input;
+
+namespace DoctorFAM.Application.CQRS.SiteSide.BloodPressure.Commands.AddBloodPressurePopulation;
+
+public record AddBloodPressurePopulationCommand : IRequest<bool>
+{
+    public BloodPressurePopulationDTO command { get; set; }
+}
